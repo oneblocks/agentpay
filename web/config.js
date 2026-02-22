8eb40ae1b4949d195785331d35217e2c7b2cba4b
kun <ertou2017@gmail.com>
2026-02-22 23:38:55 +0800

    MVP completed.

---
+++ web/config.js
@@ -0,0 +1,9 @@
+const AGENT_CONFIG = {
+    RPC_URL: "https://testnet-rpc.monad.xyz",
+    CHAIN_ID: "0x279f", // 10143
+    CHAIN_NAME: "Monad Testnet",
+    NATIVE_SYMBOL: "MON",
+    EXPLORER_URL: "https://testnet.monadexplorer.com/",
+    USDC_ADDRESS: "0x534b2f3A21130d7a60830c2Df862319e593943A3",
+    BACKEND_URL: "http://82.156.75.205:8080"
+};
